7d89a5dcc107dcaf072b621c4196ce7aeb871bb8
Mike Keen <mwk@mikekeen.com>
2020-03-13 14:29:44 -0400

    beefier couch

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,7 +52,7 @@ variable "cluster_makeup" {
     }
 
     couchdb = {
-      couch_size = "s-1vcpu-1gb"
+      couch_size = "s-2vcpu-2gb"
       proxy_size = "s-1vcpu-1gb"
       proxy_provisioned = true
       haproxy_domain = "couchdb"
