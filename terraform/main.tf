9c84024db9e851a2c91a2f588aa47c436e6088fd
Mike Keen <mwk@mikekeen.com>
2020-03-23 16:47:55 -0400

    couchdb scale up

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,7 +56,7 @@ variable "cluster_makeup" {
       proxy_size = "s-1vcpu-1gb"
       proxy_provisioned = true
       haproxy_domain = "couchdb"
-      node_count = 3
+      node_count = 5
       disk_size = 30
     }
 
